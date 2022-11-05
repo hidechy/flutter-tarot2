af1e2197772f89b2b926bc973db8f83beda4650c
toyohide <hide.toyoda@gmail.com>
2022-11-05 22:44:07 +0900

    scroll list controll

--- lib/screens/tarot_history_screen.dart
+++ lib/screens/tarot_history_screen.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
 
 import '../layouts/default_layout.dart';
+import '../model/tarot_history.dart';
 import '../viewmodel/tarot_history_screen.dart';
 
 class TarotHistoryScreen extends ConsumerWidget {
@@ -10,10 +13,14 @@ class TarotHistoryScreen extends ConsumerWidget {
 
   final autoScrollController = AutoScrollController();
 
+  List<Map<String, dynamic>> buttonList = [];
+
   ///
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tarotHistoryState = ref.watch(tarotHistoryProvider);
+
+    makeJumpButton(data: tarotHistoryState.record);
 
     final size = MediaQuery.of(context).size;
 
@@ -36,31 +43,64 @@ class TarotHistoryScreen extends ConsumerWidget {
               ),
             ],
           ),
-          Container(
+          SizedBox(
             width: double.infinity,
             height: size.height * 0.2,
-            child: Wrap(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    autoScrollController.scrollToIndex(100);
-                  },
-                  child: const Text('jump'),
-                ),
-              ],
+            child: SingleChildScrollView(
+              child: Wrap(
+                children: buttonList.map((val) {
+                  return GestureDetector(
+                    onTap: () {
+                      autoScrollController.scrollToIndex(
+                        int.parse(val['pos'].toString()),
+                        preferPosition: AutoScrollPosition.begin,
+                      );
+                    },
+                    child: Container(
+                      width: size.width / 5,
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 5,
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.yellowAccent.withOpacity(0.2),
+                      ),
+                      child: Text(
+                        val['ym'].toString(),
+                        style: const TextStyle(fontSize: 12),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
             ),
           ),
           SizedBox(
             height: size.height * 0.7 - 80,
             child: ListView.builder(
+              padding: EdgeInsets.zero,
               controller: autoScrollController,
               itemCount: tarotHistoryState.record.length,
               itemBuilder: (context, index) {
+                final date =
+                    '${tarotHistoryState.record[index].year}-${tarotHistoryState.record[index].month}-${tarotHistoryState.record[index].day}';
+
                 return AutoScrollTag(
                   key: ValueKey(index),
                   index: index,
                   controller: autoScrollController,
-                  child: Text(tarotHistoryState.record[index].name),
+                  child: Card(
+                    color: Colors.black.withOpacity(0.1),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(date),
+                        Text(tarotHistoryState.record[index].name),
+                      ],
+                    ),
+                  ),
                 );
               },
             ),
@@ -68,5 +108,20 @@ class TarotHistoryScreen extends ConsumerWidget {
         ],
       ),
     );
+  }
+
+  ///
+  void makeJumpButton({required List<TarotHistory> data}) {
+    var keepYm = '';
+    for (var i = 0; i < data.length; i++) {
+      if ('${data[i].year}-${data[i].month}' != keepYm) {
+        final map = <String, dynamic>{};
+        map['pos'] = i;
+        map['ym'] = '${data[i].year}-${data[i].month}';
+        buttonList.add(map);
+      }
+
+      keepYm = '${data[i].year}-${data[i].month}';
+    }
   }
 }
